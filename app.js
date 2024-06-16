0eb97e0ecd964c14adc42587a0dba6fd0429468d
Nonso Anetoh <anet0001@algonquinlive.com>
2024-06-16 16:51:32 -0400

    updates: application

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(favicon("public/meta/favicon.ico"));
+app.use(favicon("meta/favicon.ico"));
 
 if (process.env.NODE_ENV === "development") {
   app.use(errorHandler());
